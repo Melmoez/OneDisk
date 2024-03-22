339c1cdcfc14ccf54afbedec08dab5ce43cb9e52
melmoes <ilyamelnikov123@yandex.ru>
2024-03-22 23:52:59 +0300

    file formatting to PEP-8

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 from dotenv import dotenv_values
+import os
 
-app_settings = dict(dotenv_values('../.env'))
+app_settings = dict(dotenv_values("/home/melmoes/PycharmProjects/OneDisk/.env"))
 user_data = {}
-
--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ from src import config
 from src.services.authorization import user_token
 
 
-if __name__ == '__main__':
-        print(config.user_data)
-        user_token.get_token()
+if __name__ == "__main__":
+    print(config.user_data)
+    user_token.get_token()
